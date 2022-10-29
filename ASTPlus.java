af71e616cc104ff700d23a0fe96746ef9ff169fc
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-10-30 00:04:42 +0100

    Update ASTPlus.java

--- ASTPlus.java
+++ ASTPlus.java
@@ -1,17 +1,17 @@
 public class ASTPlus implements ASTNode {
 
-ASTNode lhs, rhs;
-
-        public int eval()
-        { 
-	int v1 = lhs.eval();
-	int v2 = rhs.eval();
-        return v1+v2; 
-	}
-    
-        public ASTPlus(ASTNode l, ASTNode r)
-        {
-		lhs = l; rhs = r;
+        ASTNode lhs, rhs;
+        
+        public ASTPlus(ASTNode l, ASTNode r){
+            lhs = l;
+            rhs = r;
         }
+
+        public int eval(Environment env) {
+            int v1 = lhs.eval(env);
+            int v2 = rhs.eval(env);
+            return v1+v2;
+        }
+        
 }
 
