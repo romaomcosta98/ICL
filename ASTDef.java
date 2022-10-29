c237f850f7768b9975e5cb0dc7e458bc5b8801a4
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-10-30 00:05:05 +0100

    Update ASTDef.java

--- ASTDef.java
+++ ASTDef.java
@@ -1,12 +1,26 @@
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class ASTDef {
     
     Map<String, ASTNode> init;
     ASTNode body;
     
+    public ASTDef(Map<String, ASTNode> init, ASTNode body){
+        this.init = init;
+        this.body = body;
+    }
+
     int eval(Environment e){
-        return 0;
+        e = e.beginScope();
+        int v;
+        for(Entry<String, ASTNode> aux : init.entrySet()){
+            v = aux.getValue().eval(e);
+            e.assoc(aux.getKey(), v);
+        }
+        v = body.eval(e);
+        e = e.endScope();
+        return v;
 
     }
 
