09ca7337d959cfa770accec2d1f2ef1e67675768
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-10-30 00:05:13 +0100

    Update ASTDiv.java

--- ASTDiv.java
+++ ASTDiv.java
@@ -3,14 +3,15 @@ public class ASTDiv implements ASTNode {
     ASTNode lhs, rhs;
 
     @Override
-    public int eval() {
-       int v1 = lhs.eval();
-       int v2 = rhs.eval();
+    public int eval(Environment env) {
+       int v1 = lhs.eval(env);
+       int v2 = rhs.eval(env);
        return v1/v2;
     }
 
     public ASTDiv(ASTNode l, ASTNode r){
-		lhs = l; rhs = r;
+		  lhs = l; 
+      rhs = r;
     }
     
     
