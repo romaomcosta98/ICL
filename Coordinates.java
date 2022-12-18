6462bded7227dee7235a64fe58d7cc9f78d96b44
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-12-18 23:45:41 +0000

    Create Coordinates.java

---
+++ Coordinates.java
@@ -0,0 +1,17 @@
+public class Coordinates {
+    int depth;
+    String id;
+
+    public Coordinates(int depth, String id) {
+        this.depth = depth;
+        this.id = id;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
