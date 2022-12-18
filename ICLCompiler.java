48bb13dd4d19a0038e8cad27666e11a5b98fa3fb
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-12-18 23:45:48 +0000

    Update ICLCompiler.java

--- ICLCompiler.java
+++ ICLCompiler.java
@@ -3,14 +3,14 @@ import java.io.PrintStream;
 
 public class ICLCompiler {
     public static void main(String[] args) {
-        System.out.println("Please insert you codce to be compiled: ");
+        System.out.println("Please insert you code to be compiled: ");
         Parser parser = new Parser(System.in);
         ASTNode exp;
 
         try{
             exp = parser.Start();
             CodeBlock c = new CodeBlock();
-            exp.compile(c);
+            exp.compile(c, new Environment<>());
             c.dump(new PrintStream(new FileOutputStream("Demo.j", false)));
             System.out.println("Compilation successful!");
         } catch (Exception e) {
